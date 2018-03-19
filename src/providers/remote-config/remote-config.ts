6493fa187f2a9fd25f40960327c60e6fa63065da
Jeffrey Delaney <jeffreydelaney@Jeffreys-iMac.local>
2018-03-19 06:44:15 -0700

    update: syntax fix remote config

--- src/providers/remote-config/remote-config.ts
+++ src/providers/remote-config/remote-config.ts
@@ -1,25 +1,25 @@
-import { Injectable } from "@angular/core";
-import { Platform } from "ionic-angular";
-import { Firebase } from "@ionic-native/firebase";
-import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { Firebase } from '@ionic-native/firebase';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class RemoteConfigProvider {
   private defaults = {
-    profile_banner: "Default Hello"
+    profile_banner: 'Default Hello'
   };
 
   constructor(public firebaseNative: Firebase, public platform: Platform) {}
 
   initialize() {
-    if (this.platform.is("cordova")) {
+    if (this.platform.is('cordova')) {
       (window as any).FirebasePlugin.fetch(
         600,
         () => {
-          console.log("fetched remote config ");
+          console.log('fetched remote config ');
         },
         error => {
-          console.error("error initializing remote config", error);
+          console.error('error initializing remote config', error);
         }
       );
     }
@@ -27,19 +27,19 @@ export class RemoteConfigProvider {
 
   async getValue(key: string) {
 
-    if (this.platform.is("cordova")) {
+    if (this.platform.is('cordova')) {
       const win = window as any;
 
       const remoteVal = await new Promise((resolve, reject) => {
         win.FirebasePlugin.getValue(
-          "profile_banner",
+          key,
           value => {
+            console.log('value', value);
             resolve(value);
-            console.log("value", value);
           },
           function(error) {
+            console.log('error getting value' + error);
             reject(error);
-            console.log("error getting value" + error);
           }
         );
       });
